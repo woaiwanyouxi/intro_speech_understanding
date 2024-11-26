5ee1986c6bb73db25eba1dc0778be99cccf84b3c
fine <149949700+woaiwanyouxi@users.noreply.github.com>
2024-11-26 15:05:47 +0900

    hw9
    hw9

--- 2024_fall/lec09/homework9.py
+++ 2024_fall/lec09/homework9.py
@@ -16,5 +16,19 @@ def fourier_synthesis(num_harmonics, X, T0):
     
     x[n] = (2/N) * sum_{l=1}^{num_harmonics} |X[l*N//T0]| * cos(2*pi*l*n/T0 + angle(X[l*N//T0]))
     '''
-    raise RuntimeError("You need to change this part")
+    N = len(X)
+    x = np.zeros(N)
+    n = np.arange(N)
+    for k in range(num_harmonics):
+        l = k+1
+        mag = np.abs(X[int(l*N//T0)])
+        phase = np.angle(X[int(l*N//T0)])
+        harmonic = mag*np.cos(2*np.pi+l*n/T0+phase)
+        x += harmonic
+    return x
+                    
+                       
+                 
+        
+                     
 
